56d47edc01af999887fa0f18063b9424ea225f65
Faysal Mahmud Prem <77010266+faysalmahmudprem@users.noreply.github.com>
2025-12-06 21:51:23 +0600

    Add alarm clock functionality with sound alert
    Implemented a simple alarm clock that plays a sound at the specified time.

--- alarm-clock/alarm_clock.py
+++ alarm-clock/alarm_clock.py
@@ -1,1 +1,34 @@
+# python alarm clock
+import time
+import datetime
+import pygame
 
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "Desktop/Variable/programming/Python/iphone-ringtone.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("WAKE UP!")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+
+            is_running = False
+
+        time.sleep(1)
+
+
+
+
+if __name__ == "__main__":
+    alarm_time = input("Enter a alam time (HH:MM:SS): ")
+    set_alarm(alarm_time)
